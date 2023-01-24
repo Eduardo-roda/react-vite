afa03049124a5eb03397f10bf407d987177d8e59
Eduardo Rodriguez <eduardo.roda20@gmail.com>
2023-01-23 19:14:40 -0500

    clase de React x Vite

---
+++ src/AppEjemplo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppEjemplo = () => {
+  return (
+    <div>
+      <h1>Este es nuestro ejemplo</h1>
+    </div>
+  )
+}
+
+export default AppEjemplo
---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <nav className='container' class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Logo</a>
+                    <div class="" id="navbarNav">
+                        <p>Todos los derechos - Eduardo Rodriguez</p>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Footer
